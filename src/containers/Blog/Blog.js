e5e6b2a34eac753b17de979643ba582cb7173f4a
frasnym <frastyawan.nym@gmail.com>
2020-12-16 15:26:46 +0800

    Render Fetch Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,22 +7,26 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
+	state = {
+		posts: [],
+	};
+
 	componentDidMount() {
 		axios
 			.get("http://jsonplaceholder.typicode.com/posts")
 			.then((response) => {
-				console.log(response);
+				this.setState({ posts: response.data });
 			});
 	}
 
 	render() {
+		const posts = this.state.posts.map((post) => (
+			<Post key={post.id} title={post.title} />
+		));
+
 		return (
 			<div>
-				<section className="Posts">
-					<Post />
-					<Post />
-					<Post />
-				</section>
+				<section className="Posts">{posts}</section>
 				<section>
 					<FullPost />
 				</section>
